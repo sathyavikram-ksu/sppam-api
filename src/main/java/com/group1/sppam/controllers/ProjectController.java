9dc93fc86addb02c6d78e0219bef5879304e6937
sathya <sathya@sathyas-MacBook-Pro.local>
2019-10-24 21:57:06 -0400

    added apis for project

--- src/main/java/com/group1/sppam/controllers/ProjectController.java
+++ src/main/java/com/group1/sppam/controllers/ProjectController.java
@@ -1,8 +1,9 @@
 package com.group1.sppam.controllers;
 
-import com.group1.sppam.exception.ResourceNotFoundException;
 import com.group1.sppam.models.Project;
 import com.group1.sppam.repository.ProjectRepository;
+import com.group1.sppam.security.CurrentUser;
+import com.group1.sppam.security.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,35 +17,35 @@ public class ProjectController {
     @Autowired
     private ProjectRepository repository;
 
-    @GetMapping("")
-    Iterable<Project> all() {
-        return repository.findAll();
-    }
-
-    @GetMapping("/{id}")
-    Optional<Project> byId(@PathVariable Long id) {
-        return repository.findById(id);
-    }
-
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
     Project newProject(@Valid @RequestBody Project newProject) {
         return repository.save(newProject);
     }
 
-    @PutMapping("/{id}")
-    Project replaceEmployee(@RequestBody Project updatedProject, @PathVariable Long id) {
-        return repository.findById(id)
-                .map(project -> {
-                    project.update(updatedProject);
-                    return repository.save(project);
-                })
-                .orElseThrow(() -> new ResourceNotFoundException("Project", "id", id));
+    @GetMapping("/byuser")
+    Iterable<Project> byUser(@CurrentUser UserPrincipal currentUser) {
+        return repository.findDistinctByOwner_IdOrTeamMembers_Id(currentUser.getId(), currentUser.getId());
     }
 
-    @DeleteMapping("/{id}")
-    void deleteEmployee(@PathVariable Long id) {
-        //delete risks
-        repository.deleteById(id);
+    @GetMapping("/{id}")
+    Optional<Project> byId(@PathVariable Long id) {
+        return repository.findById(id);
     }
+//
+//    @PutMapping("/{id}")
+//    Project replaceEmployee(@RequestBody Project updatedProject, @PathVariable Long id) {
+//        return repository.findById(id)
+//                .map(project -> {
+//                    project.update(updatedProject);
+//                    return repository.save(project);
+//                })
+//                .orElseThrow(() -> new ResourceNotFoundException("Project", "id", id));
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    void deleteEmployee(@PathVariable Long id) {
+//        //delete risks
+//        repository.deleteById(id);
+//    }
 }
--- src/main/java/com/group1/sppam/repository/ProjectRepository.java
+++ src/main/java/com/group1/sppam/repository/ProjectRepository.java
@@ -3,5 +3,8 @@ package com.group1.sppam.repository;
 import com.group1.sppam.models.Project;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ProjectRepository extends CrudRepository<Project, Long> {
+    Iterable<Project> findDistinctByOwner_IdOrTeamMembers_Id(Long ownerId, Long userId);
 }
